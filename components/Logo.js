81d69ff191c596ffe7f78af627696e6300543b8c
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-09-15 13:26:52 +0200

    Update Logo.js

--- components/Logo.js
+++ components/Logo.js
@@ -3,8 +3,8 @@ import Link from "next/link"
 
 export default props => <Link href="/" {...props}>
     <picture>
-        <source media="(min-width: 768px)" srcSet="logo-desktop.svg" />
-        <img src="logo-mobile.svg" style={{height: 28}} />
+        <source media="(min-width: 768px)" srcSet="/logo-desktop.svg" />
+        <img src="/logo-mobile.svg" style={{height: 28}} />
     </picture>
     {/* <Image src="/logo-mobile.svg" sizes="(min-width: 768px) logo-desktop.png" width={28} height={28} />  */}
 </Link>
