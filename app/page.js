35268fe7b4d3880fee811b5a84ffc87e07cfe934
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-09-15 13:26:56 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,19 +1,3 @@
-import UList from "@/components/UList"
-import Link from "next/link"
+import { redirect } from "next/navigation"
 
-import { Barlow } from "next/font/google"
-
-const font = Barlow({ preload: false, weight: ["500", "700"] })
-
-export default () => (
-    <section className={`${font.className} border-y-very-light-grey border-y-[1px] text-sm px-4 md:text-base`}>
-        <div className="flex items-center md:max-w-[1400px] md:m-auto md:px-6">
-            <h1 className="uppercase font-bold border-x-very-light-grey border-x-[1px] px-[14px] py-4 tracking-widest md:px-6">home</h1>
-            <UList
-                className="flex gap-2 grow justify-end text-grey px-4 md:gap-6"
-                items={["Feeds", "My challenges", "My network", "My bookmarks"]}
-                itemHandler={item => <Link href={`/home/${item.replace(" ", "-").toLowerCase()}`}>{item}</Link>}
-            />
-        </div>
-    </section>
-)
+export default () => redirect("/home")
