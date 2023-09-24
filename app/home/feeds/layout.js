fb1b75fba80b7b37123a5d17196aac3861336d2f
md5dalton <md5daltonggs@gmail.com>
2023-09-24 14:50:04 +0200

    Update layout.js

--- app/home/feeds/layout.js
+++ app/home/feeds/layout.js
@@ -1,9 +1,22 @@
 export default ({ children }) => (
-    <article className="container py-[1.6rem] grid md:py-8 md:grid-cols-[1fr_2.5fr] md:grid-rows-[min-content_1fr]">
-        <section className="hidden md:block">sidebar-l</section>
+    <article className="container
+        py-[1.6rem]
+        grid
+
+        md:grid-cols-[1fr_2.25fr]
+        md:grid-rows-[min-content_1fr]
+        md:gap-6
+        
+        md:py-14
+        
+        lg:grid-cols-[1fr_2.25fr_1fr]
+        lg:grid-rows-none
+        lg:gap-8
+        ">
+        <section className="hidden md:block bg-red">sidebar-l</section>
         <section>
             {children}
         </section>
-        <section>sidebar-r</section>
+        <section className="border-x-very-light-grey border-x-[1px] rounded-2xl bg-white">sidebar-r</section>
     </article>
 )
