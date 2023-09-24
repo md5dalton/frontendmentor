b28c1aa0e42f5c0a3bacf293add10659e15ca013
md5dalton <md5daltonggs@gmail.com>
2023-09-24 12:51:31 +0200

    Update layout.js

--- app/home/layout.js
+++ app/home/layout.js
@@ -4,11 +4,11 @@ import { barlow } from "@/components/Fonts"
 import NavLink from "@/components/NavLink"
 
 export default ({ children }) => <>
-    <section className={`${barlow} border-y-very-light-grey border-y-[1px] text-sm px-4 md:text-base`}>
-        <div className="flex items-center md:max-w-[1400px] md:m-auto md:px-6">
-            <h1 className="uppercase font-bold border-x-very-light-grey border-x-[1px] px-[14px] py-[0.8rem] tracking-widest md:px-6">home</h1>
+    <section className={`${barlow} border-y-very-light-grey border-y-[1px] text-sm md:text-base`}>
+        <div className="flex items-center container">
+            <h1 className="uppercase font-bold border-x-very-light-grey border-x-[1px] px-[14px] py-[0.8rem] tracking-widest md:px-6 md:py-4">home</h1>
             <UList
-                className="flex gap-2 grow text-grey px-4 py-2 overflow-x-auto no-scrollbar whitespace-nowrap md:gap-6 md:justify-end"
+                className="flex gap-2 grow text-grey px-4 py-2 overflow-x-auto no-scrollbar whitespace-nowrap md:gap-6 md:justify-end md:py-4"
                 items={["Feeds", "My challenges", "My network", "My bookmarks"]}
                 itemHandler={item => <NavLink
                     activeClassName="after:scale-x-100" 
