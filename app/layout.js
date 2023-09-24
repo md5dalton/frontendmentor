43f859eb1ebee776aec9f332945f1698f496dd96
md5dalton <md5daltonggs@gmail.com>
2023-09-24 12:51:35 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="overflow-y-scroll">
       <body className={`${font.className} text-[1.6rem]`}>
         <Header />
         <main>
