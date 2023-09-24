a80ba079aeffff89cd440fcb8f0c77a82ca957cf
md5dalton <md5daltonggs@gmail.com>
2023-09-24 11:23:38 +0200

    Update layout.js

--- app/home/layout.js
+++ app/home/layout.js
@@ -10,7 +10,7 @@ export default ({ children }) => <>
         <div className="flex items-center md:max-w-[1400px] md:m-auto md:px-6">
             <h1 className="uppercase font-bold border-x-very-light-grey border-x-[1px] px-[14px] py-[0.8rem] tracking-widest md:px-6">home</h1>
             <UList
-                className="flex gap-2 grow justify-end text-grey px-4 md:gap-6"
+                className="flex gap-2 grow text-grey px-4 overflow-x-auto whitespace-nowrap md:gap-6 md:justify-end"
                 items={["Feeds", "My challenges", "My network", "My bookmarks"]}
                 itemHandler={item => <Link href={`/home/${item.replace(" ", "-").toLowerCase()}`}>{item}</Link>}
             />
