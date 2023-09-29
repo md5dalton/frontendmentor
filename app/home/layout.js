d40baadae1fa67823a974ffd5cac32ff4e6c035b
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-09-29 08:01:09 +0200

    Update layout.js

--- app/home/layout.js
+++ app/home/layout.js
@@ -4,24 +4,26 @@ import NavLink from "@/components/NavLink"
 
 export default ({ children }) => <>
     <section className={`${barlow} border-y-very-light-grey border-y-[1px] text-sm md:text-base`}>
-        <div className="flex items-center container">
+        <div className="flex items-center gap-3 container">
             <h1 className="uppercase font-bold border-x-very-light-grey border-x-[1px] px-[14px] py-[0.8rem] tracking-widest md:px-6 md:py-4">home</h1>
-            <UList
-                className="flex gap-2 grow text-grey py-2 overflow-x-auto no-scrollbar whitespace-nowrap md:gap-6 justify-end md:py-4"
-                items={["Feeds", "My challenges", "My network", "My bookmarks"]}
-                itemHandler={item => <NavLink
-                    activeClassName="after:scale-x-100" 
-                    className="after:block 
-                        after:bg-red
-                        after:h-[2.4px]
-                        after:scale-x-0
-                        after:transition-transform
-                        after:ease-in-out
-                        after:duration-300
-                        hover:after:scale-x-100"
-                    href={`/home/${item.replace(" ", "-").toLowerCase()}`}
-                >{item}</NavLink>}
-            />
+            <div className="px-3 overflow-x-auto">
+                <UList
+                    className="inline-flex gap-2 grow text-grey py-2 whitespace-nowrap md:gap-6 md:py-4"
+                    items={["Feeds", "My challenges", "My network", "My bookmarks"]}
+                    itemHandler={item => <NavLink
+                        activeClassName="after:scale-x-100" 
+                        className="after:block 
+                            after:bg-red
+                            after:h-[2.4px]
+                            after:scale-x-0
+                            after:transition-transform
+                            after:ease-in-out
+                            after:duration-300
+                            hover:after:scale-x-100"
+                        href={`/home/${item.replace(" ", "-").toLowerCase()}`}
+                    >{item}</NavLink>}
+                />
+            </div>
         </div>
     </section>
     {children}
