c6b8c07e030ee7cceb0f13ea3be3970b7332806f
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-11-13 08:52:56 +0200

    Update layout.js

--- app/home/layout.js
+++ app/home/layout.js
@@ -5,21 +5,14 @@ import NavLink from "@/components/NavLink"
 export default ({ children }) => <>
     <section className={`${barlow} border-y-very-light-grey border-y-[1px] text-sm md:text-base`}>
         <div className="flex items-center gap-3 container">
-            <h1 className="uppercase font-bold border-x-very-light-grey border-x-[1px] px-[14px] py-[0.8rem] tracking-widest md:px-6 md:py-4">home</h1>
-            <div className="px-3 overflow-x-auto">
+            <h1 className="uppercase font-bold border-x-very-light-grey border-x-[1px] p-3 tracking-widest md:px-5">home</h1>
+            <div className="px-3 overflow-x-auto ml-auto">
                 <UList
-                    className="inline-flex gap-2 grow text-grey py-2 whitespace-nowrap md:gap-6 md:py-4"
+                    className="inline-flex gap-2 grow text-grey py-2 whitespace-nowrap lg:gap-6 lg:py-4"
                     items={["Feeds", "My challenges", "My network", "My bookmarks"]}
                     itemHandler={item => <NavLink
                         activeClassName="after:scale-x-100" 
-                        className="after:block 
-                            after:bg-red
-                            after:h-[2.4px]
-                            after:scale-x-0
-                            after:transition-transform
-                            after:ease-in-out
-                            after:duration-300
-                            hover:after:scale-x-100"
+                        className="primary-link-hover"
                         href={`/home/${item.replace(" ", "-").toLowerCase()}`}
                     >{item}</NavLink>}
                 />
